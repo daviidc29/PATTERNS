628d0de8632c5f8552a6f41ed4b03e0fa75193a3
David Castro <david.csierra@mail.escuelaing.edu.co>
2025-01-29 09:45:29 -0500

    saludo personalizado

--- src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -1,10 +1,20 @@
 package edu.eci.cvds.patterns.archetype;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 0) {
+            StringBuilder name = new StringBuilder();
+            for (String arg : args) {
+                name.append(arg).append(" ");
+            }
+            System.out.println("Hello " + name.toString().trim() + "!");
+        } else {
+            System.out.println("Hello World!");
+        }
     }
 }
