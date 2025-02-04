1c39081b3c578c8233c17244a33c40648c7d54d0
David Castro <david.csierra@mail.escuelaing.edu.co>
2025-02-04 18:09:45 -0500

    fabrica terminada

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,5 +1,23 @@
 package edu.eci.cvds.patterns.shapes;
 
-public abstract class ShapeFactory {
+import edu.eci.cvds.patterns.shapes.concrete.*;
 
+public  class ShapeFactory {
+    
+
+    public static Shape create(RegularShapeType type) {
+        switch (type) {
+            case Triangle:
+                return new Triangle();
+            case Quadrilateral:
+                return new Quadrilateral();
+            case Pentagon:
+                return new Pentagon();
+            case Hexagon:
+                return new Hexagon();
+            default:
+                throw new IllegalArgumentException("Invalid shape type: " + type);
+        }
+    }
+    
 }
--- src/main/java/edu/eci/cvds/patterns/shapes/concrete/Pentagon.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/concrete/Pentagon.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.patterns.shapes.concrete;
 import edu.eci.cvds.patterns.shapes.Shape;
 
 public class Pentagon implements Shape {
+    @Override
     public int getNumberOfEdges() {
         return 5;
     }
